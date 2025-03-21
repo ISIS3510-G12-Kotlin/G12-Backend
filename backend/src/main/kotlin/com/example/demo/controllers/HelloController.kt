80801a6eb83ee3422678ad77fa5b1c941ed9168e
CamiloD-96 <ca.daza10@uniandes.edu.co>
2025-03-21 05:07:54 -0500

    Backend Structure First Draft

---
+++ backend/src/main/kotlin/com/example/demo/controllers/HelloController.kt
@@ -0,0 +1,13 @@
+package com.example.demo.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun sayHello(): String {
+        return "Hello from ExplorAndes backend!"
+    }
+}
